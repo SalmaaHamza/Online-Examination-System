fe7f0a1c72eddb99f3f0263831c814e6b93f62b2
SalmaaHamza <salmahamza108@gmail.com>
2022-01-31 06:59:11 +0200

    DS & ALgorithms Topic and Questions

---
+++ DB/Code/InsertData/Add_Topic_DSandALg.sql
@@ -0,0 +1,23 @@
+-------ADD Course-------
+spAddCourse 3,'DSandALg', 40
+GO
+------ADD Topics -------
+spAddTopic 23, 'Arrays',3 
+Go
+spAddTopic 24, 'Stack' ,3
+Go
+spAddTopic 25, 'LinkedList',3
+Go
+spAddTopic 26, 'Circular LinkedList',3
+Go
+spAddTopic 27, 'Queue',3
+Go
+spAddTopic 28, 'Binary Search',3
+Go
+spAddTopic 29, 'Binary Tree',3
+Go
+spAddTopic 30, 'Graph',3
+Go
+spAddTopic 31, 'Breadth First Search',3
+Go
+spAddTopic 32, 'Depth First Traversals',3
